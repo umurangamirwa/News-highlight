829063510593efb7a89c9d1f811bad14fcb9d3bd
Iyerikuzwe Regine <iyerikuzweregine19@gmail.com>
2019-02-18 15:27:35 +0200

    addremove app routes in views

--- app/views.py
+++ app/views.py
@@ -28,3 +28,17 @@ def index():
 
     title = 'Home - Welcome to The best News Review Website Online'
     return render_template('index.html', title = title)
+    from .request import get_movies
+
+# @app.route('/')
+def index():
+
+    '''
+    View root page function that returns the index page and its data
+    '''
+
+    # Getting popular movie
+    popular_news = get_news('popular')
+    print(popular_news)
+    title = 'Home - Welcome to The best News Review Website Online'
+    return render_template('index.html', title = title,popular = popular_news)
